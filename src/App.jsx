717af7a3fec8c0bace5a9a3584355b5e44adab23
Prabal Bhandary <prabalbhandary97@gmail.com>
2024-12-19 21:24:19 +0545

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 const App = () => {
+  const [isToken, setIsToken] = useState(false);
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={isToken ? <Home /> : <Login />} />
+        <Route path="/signup" element={!isToken ? <Signup /> : <Home />} />
+        <Route path="/login" element={!isToken ? <Login /> : <Home />} />
       </Routes>
     </div>
   );
